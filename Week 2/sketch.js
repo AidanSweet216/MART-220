a4d7c3a2a5f23173cb2e3a4addf48e7b57b2e2d3
AidanSweet216 <77294982+AidanSweet216@users.noreply.github.com>
2022-01-27 10:55:35 -0700

    week 2

---
+++ Week 2/sketch.js
@@ -0,0 +1,56 @@
+var x = 0;
+var y = 0;
+var circX = 785;
+var circY = 368;
+var squareX1 = 0;
+var squareY = 0;
+var squareX2 = 0;
+var speedX = 10;
+var speedY = 10;
+
+function setup()
+{
+    createCanvas(1920,1080);
+    squareX1 = 105;
+    squareX2 = 1143;
+    squareY = 368;
+    
+}
+function draw()
+{
+    background(220);
+    circle(circX,circY,55);
+    square(squareX1,squareY,100);
+    square(squareX2,squareY,100);
+    text(x +"and"+ y, 100, 300);
+    textSize(42);
+    text("Pong", 762, 34);
+
+    if (circX >= 1143)
+    {
+        speedX = 10;
+        speedX = -speedX;
+    }
+    else if (circX < 208)
+    {
+        speedX = 10;
+    }
+    circX = circX + speedX;
+}
+
+if (squareY >= 368)
+{
+    speedY = 10;
+    speedY = -speedY;
+}
+else if (squareY < 0)
+{
+    speedY = 10;
+}
+squareY = squareY + speedY;
+
+function mouseMoved()
+{
+    x = mouseX;
+    y = mouseY;
+}
