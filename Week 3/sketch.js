0a2914cba27e95033eef2f3e053ffae2602f6aa4
Aidan Sweet <77294982+AidanSweet216@users.noreply.github.com>
2022-02-05 20:53:08 -0700

    updated week 3

--- Week 3/sketch.js
+++ Week 3/sketch.js
@@ -10,22 +10,33 @@ var speedX = 10;
 var speedY = 10;
 var buttonX = 0;
 var buttonY = 0;
+var timerValue = 30;
 let img;
-
-//function preload() {
-  //img = loadImage('assets/ice.jpg');
-//}
+let img2;
+let myFont;
+function preload() {
+  img = loadImage('assets/ice.jpg');
+  img2 = loadImage('assets/monte.png')
+  myFont = loadFont('assets/Rowdies-Regular.ttf');
+}
 
 
 function setup()
 {
     createCanvas(1920,1080);
+    setInterval(timeIt, 1000);
     img = loadImage('assets/ice.jpg');
     squareX1 = 105;
-    squareX2 = 1143;
+    squareX2 = 1600;
     squareY1 = 368;
     buttonX = 125;
     buttonY = 850;
+    textFont(myFont);
+  textSize(36);
+  text('p5*js', 10, 50);
+  
+ 
+
     
    
 
@@ -34,19 +45,39 @@ function setup()
 function draw()
 {
     background(220);
-    image(img, 0, 0);
-    circle(circX,circY,55);
+    image(img, 0, 0,);
+    image(img2,circX,circY, 70,70);
     square(squareX1,squareY1,100);
     square(squareX2,squareY2,100);
     square(buttonX,buttonY,150);
     text(x +"and"+ y, 100, 300);
     textSize(42);
-    text("Pong", 762, 34);
+    text("Griz Pong", 762, 34);
     text("By: Aidan Sweet", 1171, 857);
     text("Back", 150, 925);
-  
 
-    if (circX >= 1143)
+    //call timer
+    //text(timer, 50, 50);
+    //timer--
+   // print(timer);
+    //if(timer == 0)
+    //{
+      //text("game over");
+     // timer++
+   // }
+   if (timerValue >= 30) {
+    text("0:" + timerValue, 815,  70);
+  }
+  if (timerValue < 30) {
+    text('0:' + timerValue, 815, 70);
+  }
+  if (timerValue == 0) {
+    text('Game Over',  750, 100);
+    speedX--
+  }
+
+  
+    if (circX >= 1600)
     {
         speedX = 10;
         speedX = -speedX;
@@ -58,18 +89,7 @@ function draw()
     circX = circX + speedX;
 
     moveSquare();
-}
-
-//if (squareY1 >= 300)
-//{
-    //speedY = 10;
-  //  speedY = -speedY;
-//}
-//else if (squareY1 < 10)
-//{
-  //  speedY = 10;
-//}
-//squareY = squareY + speedY;
+  }
 
 function mouseMoved()
 {
@@ -91,5 +111,12 @@ function moveSquare()
       {
         squareY1+=5;
       }
+      
     }
+    function timeIt() {
+      if (timerValue > 0) {
+        timerValue--;
+      }
+    }
+
 
