b5282fdcd344da1e170f4b2f68ea33cbe8a64f5b
AidanSweet216 <77294982+AidanSweet216@users.noreply.github.com>
2022-02-14 14:19:14 -0700

    week 5

--- Week 5/sketch.js
+++ Week 5/sketch.js
@@ -2,8 +2,6 @@ var x = 0;
 var y = 0;
 var circX = 785;
 var circY = 368;
-var squareX = [];
-var squareY = [];
 var speedX = 10;
 var speedY = 10;
 var timerValue = 30;
@@ -25,15 +23,12 @@ function setup()
     createCanvas(windowWidth-20, windowHeight-20);
     setInterval(timeIt, 1000);
     img = loadImage('assets/ice.jpg');
-    squareX [1] = 105;
-    squareX [2] = 1400;
-    squareX [3] = 125;
-    squareY [1] = 368;
-    squareY [2] = 850;
     textFont(myFont);
     textSize(36);
     text('p5*js', 10, 50);
-  
+    square1 = new mySquare(105,368,100);
+    square2 = new mySquare(1400,368,100);
+    square3 = new mySquare(125,600,125);
  
 
     
@@ -45,9 +40,11 @@ function draw()
 {
     background(220);
     image(myImages[0], 0, 0,);
-    //translate(0,0);
-    //rotate(90);
-    //image(myImages[1],circX,circY, 70,70);
+    square1.drawSquare();
+    square1.moveSquare();
+    square2.drawSquare();
+    square2.moveSquare();
+    square3.drawSquare();
     push();
     translate(start + x, height/2-100); 
     // then rotate the grid around the pivot point by a
@@ -61,16 +58,12 @@ function draw()
         speedX *= -1;
     }
     x+=speedX;
-
-    square(squareX[1],squareY[1],100);
-    square(squareX[2],squareY[1],100);
-    square(squareX[3],squareY[2],150);
+    //square(squareX[3],squareY[2],150);
     text(x +"and"+ y, 100, 300);
     textSize(42);
     text("Griz Pong", 650, 34);
     text("By: Aidan Sweet", 1171, 857);
-    text("Back", 150, 925);
-    moveSquare();
+    text("Back", 135, 675);
 
    if (timerValue >= 30) {
     text("0:" + timerValue, 700,  70);
@@ -79,7 +72,7 @@ function draw()
     text('0:' + timerValue, 700, 70);
   }
   if (timerValue == 0) {
-    text('Game Over',  700, 100);
+    text('Game Over',  625, 100);
     speedX = 0;
   }
 
@@ -103,22 +96,6 @@ function mouseMoved()
     y = mouseY;
 }
 
-function moveSquare()
-{
-    if(keyIsPressed)
-    {
-      
-      
-       if(key == 'w')
-      {
-        squareY[1]-=5;
-      }
-      else if(key == 's')
-      {
-        squareY[1]+=5;
-      }
-    }
-    }
     function timeIt() {
       if (timerValue > 0) {
         timerValue--;
