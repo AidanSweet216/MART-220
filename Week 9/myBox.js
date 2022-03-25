2ef8c31e05bf8339d3f9733eacbfcc609f7b2cea
AidanSweet216 <77294982+AidanSweet216@users.noreply.github.com>
2022-03-24 21:45:04 -0600

    week 9

--- Week 9/myBox.js
+++ Week 9/myBox.js
@@ -1,25 +1,30 @@
 class myBox
 {
 
-    constructor(width, height, depth, detailX, detailY, locationX, locationY)
+    constructor(width, height, depth, locationX, locationY)
     {
         this.width = width;
         this.height = height;
         this.depth = depth;
-        this.detailX = detailX;
-        this.detailY = detailY;
         this.locationX = locationX;
-        this.depth.locationY = locationY;
+        this.locationY = locationY;
     }
 
     show()
     {
-        box(this.width, this.height, this.depth, this.detailX, this.detailY); 
+        box(this.width, this.height, this.depth,); 
     }
 
     translateBox()
     {
         translate(this.locationX, this.locationY);
+    }
+
+    rotateBox(amount)
+    {
+        rotateX(amount);
+        rotateY(amount);
+
     }
 
 }
--- Week 9/sketch.js
+++ Week 9/sketch.js
@@ -1,4 +1,5 @@
 var img = [];
+var boxes = [];
 
 
 function setup()
@@ -7,32 +8,49 @@ function setup()
     img[0] = loadImage("assets/rock2.jpg")
     img[1] = loadImage("assets/rock.png")
     img[2] = loadImage("assets/rock3.jpg")
+    for(let i = 0; i < 5; i++)
+    {
+      let myB = new myBox(150,150,150,100,100);
+      let my2B = new myBox(50,50,50,400, 100);
+      let my3B = new myBox(100,100,100,200, -100);
+      boxes.push(myB);
+      boxes.push(my2B);
+      boxes.push(my3B);
+
+    }
+   
 }
 
 function draw()
 {
     background(200);
-    push();
+    for(let i = 0; i < boxes.length; i++)
+{
+  push();
+  texture(img[1]);
+  boxes[i].translateBox();
+  boxes[i].rotateBox(frameCount * .01);
+  boxes[i].show();
+  pop();
+}
+  push();
   texture(img[0]);
   translate(-100,-100);
   rotateX(frameCount * 0.01);
   rotateY(frameCount * 0.01);
-  box(150, 150, 150);
-  pop();
-
-  push();
-  texture(img[1]);
-  translate(100,100);
-  rotateX(frameCount * 0.01);
-  rotateY(frameCount * 0.01);
-  box(300, 300, 300);
+  sphere(100, 100, 100);
   pop();
 
   push();
   texture(img[2]);
-  translate(300,-200);
+  translate(300, 300);
   rotateX(frameCount * 0.01);
   rotateY(frameCount * 0.01);
-  box(200, 200, 200);
+  cone(200, 200, 200);
   pop();
+  
+  
+
+  
+
 }
